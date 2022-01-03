3419a2cad0bfd9493eb2b08cc4db0caad2e45741
GlobalEmanuel <global.emanuel@ao3tech.com>
2022-01-02 21:08:16 -0300

    Adicionar arquivos de projeto.

---
+++ TrabalhandoStrings/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TrabalhandoStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var paragrafo = "   C# é uma linguagem moderna de versátil." +
+                "Com C# consigo desenvolver para web, DeskTop, Jogos, " +
+                "Mobile, entre outros. ";
+            // Length
+            var tamanho = paragrafo.Length;
+
+            // Empty
+            var vazio = string.Empty;
+
+            // ToLower, ToUpper
+            var fraseMinuscula = paragrafo.ToLower();
+            var fraseMaiuscula = paragrafo.ToUpper();
+
+            // Split
+            var frases = paragrafo.Split('.');
+
+            // Trim, TrimEnd, TrimStart
+            var paragrafoTrim = paragrafo.Trim();
+            var paragrafoTrimEnd = paragrafo.TrimEnd();
+            var paragrafoTrimStart = paragrafo.TrimStart();
+
+            // InsNullOrWhitespace
+            var insNullOrWhitespace = string.IsNullOrWhiteSpace(paragrafo);
+
+            // Replace
+            var paragrafoCsharp = paragrafo.Replace("C#", "C-Sharp");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
